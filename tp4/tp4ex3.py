5b4f4eb127377bd4a64b5c20f94b32bb1f4e872f
iPresing <119415796+iPresing@users.noreply.github.com>
2022-12-12 13:16:34 +0100

    Updated tp4ex3

--- tp4/tp4ex3.py
+++ tp4/tp4ex3.py
@@ -5,24 +5,30 @@ value = 0
 result = 0
 
 
-while not (n in range(1,nMax + 1)):
-	n = int(input(f"Quelle est la taille de vos vecteurs [entre 1 et {nMax}] ?  "))
+#while not (n in range(1,nMax + 1)):
+#	n = int(input(f"Quelle est la taille de vos vecteurs [entre 1 et {nMax}] ?  "))
+
+while True:
+    n = int(input(f"Quelle est la taille de vos vecteurs [entre 1 et {nMax}] ? "))
+    if 1 <= n <= nMax:
+        break
 
 
-
-print("Saisie du premier vecteur : ", end="\n")
-for k2 in range(0,n):
+print("\nSaisie du premier vecteur : ", end="\n \n")
+for k2 in range(n):
 	value = int(input(f"v1[{k2}] = "))
 	v1.append(value)
-print("Saisie du second vecteur : ", end="\n")
-for k2 in range(0,n):
+
+print("\nSaisie du second vecteur : ", end="\n \n")
+for k2 in range(n):
 	value = int(input(f"v2[{k2}] = "))
 	v2.append(value)
 
-for p in range(0,n):
+
+for p in range(n):
 	result += (v1[p] * v2[p])
 
 
-print("Le produit scalaire de v1 par v2 vaut", result)
+print("\n Le produit scalaire de v1 par v2 vaut", result)
 
 
